e5640c5fe89e2307c1deb06825a4b44288ec3f9e
mariele <mariele_rbr@hotmail.com>
2021-02-08 11:13:55 -0300

    Tipos de Parametros

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express();
+
+app.use(express.json()); //falar para o express que a API vai receber informações em formato de JSON
+
 /**
  * MÉTODOS HTTP:
  * 
@@ -9,8 +12,19 @@ const app = express();
  * PUT/ PATCH: Alterar uma informação no back-end;
  * DETELE: Deletar uma informaçãono back-end;
  */
+/**
+ * TIPOS DE PARAMETROS
+ * Query Params: Filtros e paginação;
+ * Routes Params: Identificar recursos (Atualizar/Deletar)
+ * Request Body: Conteúdo na hora criar ou editar um recurso (JSON)
+ */
 
 app.get("/projects", (request, response) => {
+  //Query Params
+  const { title, owner } = request.query;
+  console.log(title);
+  console.log(owner);
+
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -18,6 +32,12 @@ app.get("/projects", (request, response) => {
 });
 
 app.post("/projects", (request, response) => {
+  // Request Body
+  const body = request.body; // ou desestruturar para pegar cada uma de forma separada
+  //const { title, owner }= request.body;
+  console.log(body);                // console.log(title);
+  // console.log(owner);
+
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -27,6 +47,10 @@ app.post("/projects", (request, response) => {
 
 // https://localhost:3333/projects/2
 app.put("/projects/:id", (request, response) => {
+  // Routes Params
+  const { id } = request.params;
+  console.log(id)
+
   return response.json([
     'Projeto 4',
     'Projeto 2',
